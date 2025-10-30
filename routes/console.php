323a8e820bfcee80f9dcf669d828271e12e9df0a
Alejandro <apadoss03@gmail.com>
2025-10-30 09:13:14 +0000

    feat(notificaciones): Añadir ejecución diaria del comando process-price-drops

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('app:process-price-drops')
+    ->dailyAt('02:00')
+    ->withoutOverlapping();
