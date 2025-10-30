401af1af61de3bac2b13e802567718448b27e032
Alejandro <apadoss03@gmail.com>
2025-10-30 09:04:35 +0000

    feat(notificaciones): Añadida vista del listado de notificaciones de un usuario

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ConfiguracionesController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\PriceNotificationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -36,3 +37,10 @@ Route::get('/configuraciones/index', [ConfiguracionesController::class, 'index']
 Route::get('/configuraciones/create', [ConfiguracionesController::class, 'create'])->name('configuraciones.create');
 Route::post('/configuraciones/store', [ConfiguracionesController::class, 'store'])->name('configuraciones.store');
 Route::get('/configuraciones/{id}/comparar/{id2?}', [ConfiguracionesController::class, 'compare'])->name('configuraciones.compare');
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/notifications/toggle', [PriceNotificationController::class, 'toggle'])->name('notifications.toggle');
+    Route::post('/notifications/deactivate', [PriceNotificationController::class, 'deactivate'])->name('notifications.deactivate');
+    Route::get('/notifications/check', [PriceNotificationController::class, 'check'])->name('notifications.check');
+    Route::get('/mis-notificaciones', [PriceNotificationController::class, 'index'])->name('notifications.index');
+});
